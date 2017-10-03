e6b250f4eee8a6760d93ec9e539632ad127af0d8
Zanith <kvl6@case.edu>
2017-10-03 16:26:40 +0700

    Remove unneeded files

--- main_events.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2015-2016 Kyle Lopin (Naresuan University) <kylel@nu.ac.th>
-# Licensed under the Creative Commons Attribution-ShareAlike  3.0 (CC BY-SA 3.0 US) License
-
-""" Functions to run when the user clicks on widgets in the main GUI window
-"""
-
-
-def change_voltage_source_event(event):
-    pass
