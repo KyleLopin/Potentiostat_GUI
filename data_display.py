187c9aa229a1321190d7a2e7f61251f31f6b8c78
Zanith <kvl6@case.edu>
2017-10-04 10:29:27 +0700

    remove unneeded files

--- data_display.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Kyle Vitautas Lopin'
-
-"""
-Custom class that will attempt to make a matplotlib graph to display data and embed the graph into a tkinter frame
-If the user does not have matplotlib or one of its dependencies installed the program will instead make a tkinter Canvas
-"""
-
-class
