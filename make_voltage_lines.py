6e667b1d552c6906ebdab2505da67815be218849
Zanith <kvl6@case.edu>
2022-09-05 12:23:45 +0700

    make voltage lines works for all conditions and has unittest for all conditions.

--- make_voltage_lines.py
+++ make_voltage_lines.py
@@ -37,8 +37,6 @@ def make_voltage_profile(start: int, end: int, increment: int,
     Returns (list): list of the voltages for the specified parameters
 
     """
-    print('make x line, make_voltage_lines')
-    print(sweep_type, start, end, increment, start_volt_type, swv_height)
     if sweep_type == "CV" and start_volt_type == "Zero":
         return make_x_line_zero_cv(start, end, increment, swv_height)
     if sweep_type == "LS":  # for linear sweep ignore what the user inputted for start_volt_type
@@ -87,8 +85,14 @@ def make_x_line_zero_cv(start, end, inc, swv_height):
         side_maker = _make_swv_side
     _line = []
     _line.extend(side_maker(0, start, inc, swv_height))
-    _line.extend(side_maker(_line[-2], end, inc, swv_height))
-    _line.extend(side_maker(_line[-2], 0, inc, swv_height))
+    next_start = _line[-2]
+    if swv_height:  # if a swv, the next start has to be modified
+        next_start = _line[-5] + int(swv_height / 2)
+    _line.extend(side_maker(next_start, end, inc, swv_height))
+    next_start = _line[-2]
+    if swv_height:  # if a swv, the next start has to be modified
+        next_start = _line[-5] + int(swv_height / 2)
+    _line.extend(side_maker(next_start, 0, inc, swv_height))
     return _line
 
 
@@ -111,17 +115,21 @@ def make_x_line_triangle(start, end, inc, swv_height):
     if swv_height:
         side_maker = _make_swv_side
     _line = []
-
     _line.extend(side_maker(start, end, inc, swv_height))
-    _line.extend(side_maker(_line[-1], start, inc, swv_height))
+    # use line[-2] because line[-1] is the peak, so start on the next on
+    # eg. [..., 58, 59, 60]; 60 is the peak so the next voltage should be 59 not 60
+    next_start = _line[-2]
+    if swv_height:  # if a swv, the next start has to be modified
+        next_start = _line[-5] + int(swv_height / 2)
+    _line.extend(side_maker(next_start, start, inc, swv_height))
     return _line
 
 
 def _make_side(start, end, increment, *args):
     """
     Helper function to make the voltages for the voltage axis (x)
-    for cyclic voltammetry and linear sweep experiments.  Will autocorrect
-    and error in the increment sign.
+    for cyclic voltammetry and linear sweep experiments.
+    Increment does not have to be negative.
     Args:
         start (int):  value to start at
         end (int):  value to end
@@ -141,26 +149,38 @@ def _make_side(start, end, increment, *args):
         while start >= end:
             lut.append(start)
             start -= increment
-    print(f"returning lut: {lut}")
     return lut
 
 
-def _make_swv_side(start, end, inc, pulse_height):
-    inc = abs(inc)
+def _make_swv_side(start, end, increment, pulse_height):
+    """
+    Helper function to make the voltages for the voltage axis (x)
+    for cyclic voltammetry and linear sweep experiments with a square wave pulse
+    superimposed.  Increment does not have to be negative.
+    Args:
+        start (int):  value to start at
+        end (int):  value to end
+        increment (int):  step size
+        pulse_height (int):  height of square wave
+
+    Returns:list of voltages from start to end in increment steps
+    with a square wave superimposed
+
+    """
+    increment = abs(increment)
     lut = []
-    print(f"making swv side with start: {start}, end: {end}, increment: {inc}")
     half_pulse = int(pulse_height / 2)
-    if start < end:
+    if start <= end:
         while start <= end:
             lut.extend(2 * [(start + half_pulse)])
             lut.extend(2 * [(start - half_pulse)])
-            start += inc
+            start += increment
     elif start > end:
         while start >= end:
             lut.extend(2 * [(start + half_pulse)])
             lut.extend(2 * [(start - half_pulse)])
-            start -= inc
-    print(f"returning swv line: {lut}")
+            start -= increment
+    # print(f"returning swv line: {lut}")
     return lut
 
 
@@ -205,12 +225,156 @@ class TestProfiles(unittest.TestCase):
                                  "going from 0 to -50")
 
     def test_make_swv_side2(self):
+        """
+        Test if going from -60 to 40 in increments of 20 and a swv height of 50 works
+        """
         line = _make_swv_side(-60, 40, 20, 50)
         correct_line = [-35, -35, -85, -85, -15, -15, -65, -65, 5, 5, -45,
                         -45, 25, 25, -25, -25, 45, 45, -5, -5, 65, 65, 15, 15]
         self.assertListEqual(line, correct_line,
                              msg=f"_make_swv_line not returning correct list for"
                                  "going from 60 to 40 in 20 increments")
+
+    def test_make_side_1(self):
+        """
+        Test if going from -60 to 40 in increments of 20 and a swv height of 50 works
+        """
+        line = _make_side(-60, 40, 20)
+        correct_line = [-60, -40, -20, 0, 20, 40]
+        self.assertListEqual(line, correct_line,
+                             msg=f"_make_line not returning correct list for"
+                                 "going from -60 to 40 in 20 increments")
+
+    def test_make_side_2(self):
+        """
+        Test if going from 60 to -100 in increments of 20 and a swv height of 50 works
+        """
+        line = _make_side(60, -100, 20)
+        correct_line = [60, 40, 20, 0, -20, -40, -60, -80, -100]
+        self.assertListEqual(line, correct_line,
+                             msg=f"_make_line not returning correct list for"
+                                 "going from 60 to -100 in 20 increments")
+
+    def test_make_cv_1(self):
+        """ Test if cyclic voltammetry profile is correct """
+        line = make_voltage_profile(-60, 20, 20, sweep_type="CV",
+                                    start_volt_type="Start")
+        correct_line = [-60, -40, -20, 0, 20, 0, -20, -40, -60]
+        self.assertListEqual(line, correct_line,
+                             msg=f"CV profile not correct going "
+                                 f"from 60 to -60 in 20 increments")
+
+    def test_make_cv_2(self):
+        """ Test if cyclic voltammetry profile is correct """
+        line = make_voltage_profile(20, -60, 20, sweep_type="CV",
+                                    start_volt_type="Start")
+        correct_line = [20, 0, -20, -40, -60, -40, -20, 0, 20]
+        self.assertListEqual(line, correct_line,
+                             msg=f"CV profile not correct going "
+                                 f"from 60 to -60 in 20 increments")
+
+    def test_make_cv_zero_1(self):
+        """ Test if cyclic voltammetry profile is correct starting at 0 V """
+        line = make_voltage_profile(-60, 20, 20, sweep_type="CV",
+                                    start_volt_type="Zero")
+        correct_line = [0, -20, -40, -60, -40, -20, 0, 20, 0]
+        self.assertListEqual(line, correct_line,
+                             msg=f"CV profile starting at 0 V is not correct going "
+                                 f"from 60 to -60 in 20 increments")
+
+    def test_make_cv_zero_2(self):
+        """ Test if cyclic voltammetry profile is correct starting at 0 V """
+        line = make_voltage_profile(40, -60, 20, sweep_type="CV",
+                                    start_volt_type="Zero")
+        correct_line = [0, 20, 40, 20, 0, -20, -40, -60, -40, -20, 0]
+        self.assertListEqual(line, correct_line,
+                             msg=f"CV profile starting at 0 V not correct going "
+                                 f"from 60 to -60 in 20 increments")
+
+    def test_make_ls_1(self):
+        """ Test if linear sweep profile is correct """
+        line = make_voltage_profile(-60, 20, 10, sweep_type="LS",
+                                    start_volt_type="Start")
+        correct_line = [-60, -50, -40, -30, -20, -10, 0, 10, 20]
+        self.assertListEqual(line, correct_line,
+                             msg=f"LS profile not correct going "
+                                 f"from 60 to -60 in 10 increments")
+
+    def test_make_ls_2(self):
+        """ Test if linear sweep profile is correct """
+        line = make_voltage_profile(20, -60, 10, sweep_type="LS",
+                                    start_volt_type="Zero")
+        correct_line = [20, 10, 0, -10, -20, -30, -40, -50, -60]
+        self.assertListEqual(line, correct_line,
+                             msg=f"LS profile not correct going "
+                                 f"from 60 to -60 in 10 increments")
+
+    def test_make_cv_swv_1(self):
+        """ Test if cyclic voltammetry with a square wave profile is correct """
+        line = make_voltage_profile(-60, 20, 20, sweep_type="CV",
+                                    start_volt_type="Start", swv_height=50)
+        correct_line = [-35, -35, -85, -85, -15, -15, -65, -65, 5, 5, -45, -45,
+                        25, 25, -25, -25, 45, 45, -5, -5, 25, 25, -25, -25, 5, 5,
+                        -45, -45, -15, -15, -65, -65, -35, -35, -85, -85]
+        self.assertListEqual(line, correct_line,
+                             msg=f"CV profile with square wave pulses not "
+                                 f"correct going from 60 to -60 in 20 increments")
+
+    def test_make_cv_swv_2(self):
+        """ Test if cyclic voltammetry with a square wave profile is correct """
+        line = make_voltage_profile(20, -60, 20, sweep_type="CV",
+                                    start_volt_type="Start", swv_height=50)
+        correct_line = [45, 45, -5, -5, 25, 25, -25, -25, 5, 5, -45, -45, -15,
+                        -15, -65, -65, -35, -35, -85, -85, -15, -15, -65, -65,
+                        5, 5, -45, -45, 25, 25, -25, -25, 45, 45, -5, -5]
+        self.assertListEqual(line, correct_line,
+                             msg=f"CV profile with square wave pulses not "
+                                 f"correct going from 60 to -60 in 20 increments")
+
+    def test_make_cv_zero_swv_1(self):
+        """ Test if cyclic voltammetry with a square wave profile
+        is correct starting at 0 V """
+        line = make_voltage_profile(-60, 20, 20, sweep_type="CV",
+                                    start_volt_type="Zero", swv_height=50)
+        correct_line = [25, 25, -25, -25, 5, 5, -45, -45, -15, -15, -65, -65,
+                        -35, -35, -85, -85, -15, -15, -65, -65, 5, 5, -45, -45,
+                        25, 25, -25, -25, 45, 45, -5, -5, 25, 25, -25, -25]
+
+        self.assertListEqual(line, correct_line,
+                             msg=f"CV profile starting at 0 V is not correct going "
+                                 f"from 60 to -60 in 20 increments with swv")
+
+    def test_make_cv_zero_swv_2(self):
+        """ Test if cyclic voltammetry with a square wave profile
+        is correct starting at 0 V """
+        line = make_voltage_profile(40, -60, 20, sweep_type="CV",
+                                    start_volt_type="Zero", swv_height=50)
+        correct_line = [25, 25, -25, -25, 45, 45, -5, -5, 65, 65, 15, 15, 45, 45, -5, -5,
+                        25, 25, -25, -25, 5, 5, -45, -45, -15, -15, -65, -65, -35, -35,
+                        -85, -85, -15, -15, -65, -65, 5, 5, -45, -45, 25, 25, -25, -25]
+        self.assertListEqual(line, correct_line,
+                             msg=f"CV profile starting at 0 V not correct going "
+                                 f"from 60 to -60 in 20 increments with swv")
+
+    def test_make_ls_swv_1(self):
+        """ Test if linear sweep profile is correct """
+        line = make_voltage_profile(-60, 20, 20, sweep_type="LS",
+                                    start_volt_type="Start", swv_height=50)
+        correct_line = [-35, -35, -85, -85, -15, -15, -65, -65, 5, 5, -45, -45,
+                        25, 25, -25, -25, 45, 45, -5, -5]
+        self.assertListEqual(line, correct_line,
+                             msg=f"LS profile not correct going "
+                                 f"from 60 to -60 in 10 increments with swv")
+
+    def test_make_ls_swv_2(self):
+        """ Test if linear sweep profile is correct """
+        line = make_voltage_profile(20, -60, 20, sweep_type="LS",
+                                    start_volt_type="Zero", swv_height=50)
+        correct_line = [45, 45, -5, -5, 25, 25, -25, -25, 5, 5, -45, -45, -15,
+                        -15, -65, -65, -35, -35, -85, -85]
+        self.assertListEqual(line, correct_line,
+                             msg=f"LS profile not correct going "
+                                 f"from 60 to -60 in 10 increments with swv")
 
 
 if __name__ == "__main__":
