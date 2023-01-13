9c532518d7cb4823bf8296c5cc6bc0a83e4c7e82
KyleLopin <kvl6@case.edu>
2023-01-13 07:07:56 +0700

    Added a small serial monitor program to test the device with

---
+++ tools/serial_monitor.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2022 Kyle Lopin (Naresuan University) <kylel@nu.ac.th>
+
+"""
+Make a simple GUI to put in inputs, in an entry box and receive messages and print them
+to the console
+"""
+
+__author__ = "Kyle Vitautas Lopin"
+
+# standard libraries
+import time
+import tkinter as tk
+
+# local files
+import usb_comm
+
+
+class Monitor(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.device = usb_comm.SerialComm()
+        self.entry = tk.Entry(self)
+        self.entry.pack()
+        tk.Button(self, text="Send message").pack()
+        self.poll_for_input()
+
+    def send_message(self):
+        self.device.write_data(self.entry.get())
+
+    def poll_for_input(self):
+        self.after(1000, self.poll_for_input)
+        data = self.device.poll_for_data()
+        if data:
+            print(data)
+
+
+if __name__ == '__main__':
+    app = Monitor()
+    print("check1")
+    app.geometry("400x400")
+    app.mainloop()
